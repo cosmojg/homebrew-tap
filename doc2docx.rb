a72f3ce045499dc4a0585d52558792168a90b4c4
Cosmo <cosmo@cosmo.red>
2023-03-20 11:24:07 -0400

    Use venv install function

--- doc2docx.rb
+++ doc2docx.rb
@@ -8,7 +8,7 @@ class Doc2docx < Formula
   license "MIT"
   head "https://github.com/cosmojg/doc2docx"
 
-  depends_on "python"
+  depends_on "python@3.x"
 
   resource "appscript" do
     url "https://files.pythonhosted.org/packages/8a/9e/fe193f177dca67e5bfd70593568a85a195f93e39347f5719edf3938e5f08/appscript-1.2.2.tar.gz"
@@ -31,17 +31,7 @@ class Doc2docx < Formula
   end
 
   def install
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
-    resources.each do |r|
-      r.stage do
-        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
-      end
-    end
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    virtualenv_install_with_resources
   end
 
   test do
